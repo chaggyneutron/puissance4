78ec62f0a075dc5ca0b87f824e2786f4982df67d
chaggyneutron <achaguiaymen93@gmail.com>
2026-03-19 17:10:24 +0100

    feat: initial spring boot setup

---
+++ src/test/java/com/enseeiht/puissance4/Puissance4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enseeiht.puissance4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Puissance4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
